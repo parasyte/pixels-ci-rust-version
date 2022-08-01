ab84e0697c1164c14b7e296ce1f17d4ac1b7ac39
Jay Oster <jay@kodewerx.org>
2022-08-01 14:32:36 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
+use pixels::{Error, Pixels, SurfaceTexture};
+
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+
+fn main() -> Result<(), Error> {
+    let window = pixels_mocks::Rwh;
+    let mut pixels = {
+        let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    pixels.render()?;
+    pixels.resize_surface(WIDTH, HEIGHT);
+
+    Ok(())
+}
